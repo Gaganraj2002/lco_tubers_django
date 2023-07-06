0de421e2c7900dadd52918f7e554a1a1a9c6d8b3
Gagan Raj <gaganraj2002@gmail.com>
2023-07-06 23:29:19 +0530

    finished 8th topic

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -5,8 +5,15 @@ from .models import Youtuber
 
 def youtubers(request):
     tubers = Youtuber.objects.all()
+    city_search = Youtuber.objects.values_list("city",flat=True).distinct()
+    camera_search = Youtuber.objects.values_list("camera_type",flat=True).distinct()
+    category_search = Youtuber.objects.values_list("category",flat=True).distinct()
+
     data = {
-        "tubers":tubers
+        "tubers":tubers,
+        "city_search":city_search,
+        "camera_search":camera_search,
+        "category_search":category_search
     }
     return render(request,"youtubers/youtubers.html",data)
 
@@ -22,13 +29,36 @@ def youtubers_id(request, id):
 
 def search(request):
     tuber = Youtuber.objects.all()
+    city_search = Youtuber.objects.values_list("city",flat=True).distinct()
+    camera_search = Youtuber.objects.values_list("camera_type",flat=True).distinct()
+    category_search = Youtuber.objects.values_list("category",flat=True).distinct()
+
     if "Keyword" in request.GET:
         keyword = request.GET["Keyword"]
         if keyword:
             tuber= tuber.filter(description__icontains = keyword) 
             print(tuber)
+    if 'city' in request.GET:
+        city = request.GET["city"]
+        if city and city!="City":
+            tuber= tuber.filter(city__iexact = city) 
+            print(tuber)
+    if 'camera_type' in request.GET:
+        camera = request.GET["camera_type"]
+        if camera and camera!="Camera Type":
+            tuber= tuber.filter(camera_type__iexact = camera) 
+            print(tuber)
+    if 'category' in request.GET:
+        category = request.GET["category"]
+        if category and category!="Category":
+            tuber= tuber.filter(category__iexact = category) 
+            print(tuber)
+    
     data = {
-        "tubers":tuber
+        "tubers":tuber,
+        "city_search":city_search,
+        "camera_search":camera_search,
+        "category_search":category_search
     }
     return render(request,"youtubers/key_search.html",data)
 
